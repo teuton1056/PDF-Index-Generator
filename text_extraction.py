29dde0e7c32c03064bf227c24ca2cfa8c4babd32
teuton1056 <kingswarrior08@gmail.com>
2020-10-23 20:53:45 -0500

    added some comments

--- text_extraction.py
+++ text_extraction.py
@@ -7,6 +7,7 @@ import sys
 # some alterations have been made to the original files, but the core design and much content remains the same.
     
 def extract_references(text,index_file='index_files.json',index_directory="JSON"):
+    # this is a deprecated function which is retained in the code because it may prove useful in future.
     garbage = """[]{}(),.;':-_\\|"`~<>/?ﬁ"""
     words = text.split(' ')
     clean_words = []
@@ -15,7 +16,6 @@ def extract_references(text,index_file='index_files.json',index_directory="JSON"
         clean_words.append(word.strip(garbage).lstrip(garbage))
     with open(index_file,'r') as fp:
         index_files = json.load(fp)
-
     for index in index_files:
         with open(f"{index_directory}/{index}",'r') as fp:
             idx = json.load(fp)
@@ -42,41 +42,34 @@ def get_idx_for_page(text,page_number,index_file='index_files.json',index_direct
         # idx contains a list of dicts where "name" is the regular name and "aliases" are the valid abbreviations.
         for item in idx:
             for alias in item['aliases']:
+                # use a regex to find the referecnes
                 matches = re.findall(f"{alias}" + r"\.?\s\d{1,3}\.?:?\d{0,3}-?\d{0,3}", text)
                 for m in matches:
+                    # we are only interested in the numerical part of the match.
                     match = m.strip('.:;,').lstrip()[len(alias):]
                     idx_text += f"{item['name']}|{match}|{page_number}\n"
     return idx_text
 
 def create_idx(fname,base_number=1):
-
     # creating a pdf file object
     pdfFileObj = open(fname, 'rb')  
-    
     # creating a pdf reader object  
     pdfReader = PyPDF2.PdfFileReader(pdfFileObj)  
-    
     # printing number of pages in pdf file  
     #print(pdfReader.numPages)  
     number_of_pages = pdfReader.numPages
-    
     document = []
-
     # creating a page object  
     for number in range(number_of_pages):
         pageObj = pdfReader.getPage(number)  
-    
         # extracting text from page  
         text = pageObj.extractText()
         #with open(f'out_{number}.txt','w',encoding='utf-8') as fp:
         #    fp.write(text)
         page = {'Relative_Number':int(number) + base_number,'raw_text':text}
-
         document.append(page)
-    
     # closing the pdf file object  
-    pdfFileObj.close()  
-
+    pdfFileObj.close()
     # lets make an index!
     index = ""
     for page in document:
