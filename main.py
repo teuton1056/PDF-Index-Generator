4b49920a09d1c1554fd20ade027b19ce62407d6e
teuton1056 <kingswarrior08@gmail.com>
2023-08-05 09:04:35 -0500

    basic main function

---
+++ main.py
@@ -0,0 +1,12 @@
+import intake
+import reference_parser
+from index_loggers import main_logger 
+
+def main():
+    main_logger.info("Starting program")
+    intake_obj = intake.Intake()
+    document = intake_obj.load_pdf_file("test.pdf")
+    parser = reference_parser.Main_Parser()
+    refs = parser.parse(document)
+    # ... do stuff with refs
+    main_logger.info("Program complete")
