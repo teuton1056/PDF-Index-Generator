9b12d8cb6b07f14dbaf0ab0064b76ccbd5db5c1c
teuton1056 <kingswarrior08@gmail.com>
2023-08-05 10:49:02 -0500

    changes main.py to parse_lines instead of parse

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ def main():
     intake_obj = intake.Intake_PDF()
     document = intake_obj.load_pdf_file("test.pdf")
     parser = reference_parser.Main_Parser()
-    refs = parser.parse(document)
+    refs = parser.parse_lines(document)
     # ... do stuff with refs
     main_logger.info("Program complete")
