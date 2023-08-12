c2a7ec8d7910cb8446f69d0a9feeda25cc194c2e
teuton1056 <kingswarrior08@gmail.com>
2023-08-12 18:17:17 -0500

    some improved comments

--- intake.py
+++ intake.py
@@ -3,10 +3,16 @@ from index_loggers import intake_logger
 
 class Intake_PDF:
 
-    def __init__(self, base_number=0):
+    def __init__(self, base_number = 0):
         self.base_number = base_number
+        assert isinstance(self.base_number, int), "Base number must be an integer"
 
-    def load_pdf_file(self, file_path):
+    def load_pdf_file(self, file_path: str) -> list:
+        """
+        Takes the path to a file and returns the text of each page in a list of dictionaries.
+        a page has three keys, 'Raw_Number', 'Relative_Number' and 'Raw_Text'
+        Of these, the first two are ints and the last is a string.
+        """
         intake_logger.info(f"Loading file: {file_path}")
         pdfFileObj = open(file_path, 'rb')  
         pdfReader = PyPDF2.PdfReader(pdfFileObj)  
@@ -18,9 +24,11 @@ class Intake_PDF:
             # extracting text from page  
             text = pageObj.extract_text()
             # log the amount of text extracted
-            intake_logger.debug(f"Page {number} text length: {len(text)}")
+            intake_logger.debug(f"Page {number} text length: {len(text)}") # the length is in chars
+            if len(text) < 100:
+                intake_logger.warning(f"Page {number} text length is less than 100 chars. Text: {text}")
             page = {'Raw_Number':int(number), 'Relative_Number':int(number) + self.base_number,'Raw_Text':text}
-            document.append(page)
+            document.append(page) # each page is a dictionary
         # closing the pdf file object  
         pdfFileObj.close()
         intake_logger.debug(f"Document loaded. Number of pages: {len(document)}")
