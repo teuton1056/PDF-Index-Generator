7a0459c2710f51456ce7a415a73e114c88fe15d6
teuton1056 <kingswarrior08@gmail.com>
2023-08-16 10:03:07 -0500

    added dict like behavior to models

--- Index_Models.py
+++ Index_Models.py
@@ -20,6 +20,48 @@ class Mishnah_Tosefta_Reference:
         if type(__value) is Mishnah_Tosefta_Reference:
             return self.__dict__() == __value.__dict__()
         return False
+    
+    def __neq__(self, __value: object) -> bool:
+        return not self.__eq__(__value)
+    
+    def __hash__(self):
+        return hash((self.type,self.tractate,self.chapter,self.paragraph))
+    
+    def __setitem__(self, key, value):
+        if key == 'type':
+            self.type = value
+        elif key == 'tractate':
+            self.tractate = value
+        elif key == 'chapter':
+            self.chapter = value
+        elif key == 'paragraph':
+            self.paragraph = value
+        else:
+            raise KeyError(f"Invalid key {key}")
+        
+    def __getitem__(self, key):
+        if key == 'type':
+            return self.type
+        elif key == 'tractate':
+            return self.tractate
+        elif key == 'chapter':
+            return self.chapter
+        elif key == 'paragraph':
+            return self.paragraph
+        else:
+            raise KeyError(f"Invalid key {key}")
+    
+    def __iter__(self):
+        return iter(self.__dict__())
+    
+    def __contains__(self, item):
+        return item in self.__dict__() 
+    
+    def __len__(self):
+        return len(self.__dict__())
+    
+    def keys(self):
+        return self.__dict__().keys()
 
 class Talmud_Reference:
 
@@ -44,6 +86,48 @@ class Talmud_Reference:
         if type(__value) is Talmud_Reference:
             return self.__dict__() == __value.__dict__()
         return False
+    
+    def __neq__(self, __value: object) -> bool:
+        return not self.__eq__(__value)
+    
+    def __hash__(self):
+        return hash((self.type,self.tractate,self.folio,self.page_letter))
+    
+    def __setitem__(self, key, value):
+        if key == 'type':
+            self.type = value
+        elif key == 'tractate':
+            self.tractate = value
+        elif key == 'folio':
+            self.folio = value
+        elif key == 'page_letter':
+            self.page_letter = value
+        else:
+            raise KeyError(f"Invalid key {key}")
+        
+    def __getitem__(self, key):
+        if key == 'type':
+            return self.type
+        elif key == 'tractate':
+            return self.tractate
+        elif key == 'folio':
+            return self.folio
+        elif key == 'page_letter':
+            return self.page_letter
+        else:
+            raise KeyError(f"Invalid key {key}")
+        
+    def __iter__(self):
+        return iter(self.__dict__())
+    
+    def __contains__(self, item):
+        return item in self.__dict__() 
+    
+    def __len__(self):
+        return len(self.__dict__())
+    
+    def keys(self):
+        return self.__dict__().keys()
 
 class DSS_Reference:
 
@@ -74,7 +158,56 @@ class DSS_Reference:
         if type(__value) is DSS_Reference:
             return self.__dict__() == __value.__dict__()
         return False
+    
+    def __neq__(self, __value: object) -> bool:
+        return not self.__eq__(__value)
+    
+    def __hash__(self):
+        return hash((self.Cave_Number,self.Manuscript_Designator,self.Column,self.Line,self.Segment_Indicator,self.Use_Capital_Columns))
+    
+    def __setitem__(self, key, value):
+        if key == 'Cave_Number':
+            self.Cave_Number = value
+        elif key == 'Manuscript_Designator':
+            self.Manuscript_Designator = value
+        elif key == 'Column':
+            self.Column = value
+        elif key == 'Line':
+            self.Line = value
+        elif key == 'Segment_Indicator':
+            self.Segment_Indicator = (value)
+        elif key == 'Use_Capital_Columns':
+            self.Use_Capital_Columns = (value == 'True' or value == True)
+        else:
+            raise KeyError(f"Invalid key {key}")
 
+    def __getitem__(self, key):
+        if key == 'Cave_Number':
+            return self.Cave_Number
+        elif key == 'Manuscript_Designator':
+            return self.Manuscript_Designator
+        elif key == 'Column':
+            return self.Column
+        elif key == 'Line':
+            return self.Line
+        elif key == 'Segment_Indicator':
+            return self.Segment_Indicator
+        elif key == 'Use_Capital_Columns':
+            return self.Use_Capital_Columns
+        else:
+            raise KeyError(f"Invalid key {key}")
+
+    def __iter__(self):
+        return iter(self.__dict__())
+    
+    def __contains__(self, item):
+        return item in self.__dict__() 
+    
+    def __len__(self):
+        return len(self.__dict__())
+    
+    def keys(self):
+        return self.__dict__().keys()
 class Bible_Reference:
 
     def __init__(self, book, chapter, verse):
@@ -87,7 +220,30 @@ class Bible_Reference:
             return f"{self.book} {self.verse}" # for one chapter books 
         else:
             return f"{self.book} {self.chapter}:{self.verse}"
+    
+    def __hash__(self):
+        return hash((self.book,self.chapter,self.verse))
+    
+    def __setitem__(self, key, value):
+        if key == 'book':
+            self.book = value
+        elif key == 'chapter':
+            self.chapter = value
+        elif key == 'verse':
+            self.verse = value
+        else:
+            raise KeyError(f"Invalid key {key}")
         
+    def __getitem__(self, key):
+        if key == 'book':
+            return self.book
+        elif key == 'chapter':
+            return self.chapter
+        elif key == 'verse':
+            return self.verse
+        else:
+            raise KeyError(f"Invalid key {key}") 
+
     def __repr__(self):
         return "Bible_Reference({},{},{})".format(self.book,self.chapter,self.verse)
     
@@ -102,17 +258,14 @@ class Bible_Reference:
     def __neq__(self, __value: object) -> bool:
         return not self.__eq__(__value)
 
-class Index_Entry:
-
-    def __init__(self, reference, page_number: str):
-        self.reference = reference # must be a reference object, one of the above
-        self.page_number = page_number
-
-    def __str__(self):
-        return f"{self.reference} {self.page_number}"
+    def __iter__(self):
+        return iter(self.__dict__())
     
-    def __repr__(self):
-        return f"Index_Entry({self.reference},{self.page_number})"
+    def __contains__(self, item):
+        return item in self.__dict__() 
     
-    def __dict__(self):
-        return {'reference':dict(self.reference), 'page_number':str(self.page_number)}
+    def __len__(self):
+        return len(self.__dict__())
+    
+    def keys(self):
+        return self.__dict__().keys()
